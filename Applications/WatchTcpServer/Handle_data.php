12f4ee5cdf20fac0e6c817d100eafbed772d4b46
wzb <wangzhibin_x@qq.com>
2016-08-06 14:39:09 +0800

    修改了接收数据过长的处理

--- Applications/WatchTcpServer/Handle_data.php
+++ Applications/WatchTcpServer/Handle_data.php
@@ -9,105 +9,86 @@ class handle_data
     * @ 处理手表终端发送过来的数据
     * LENGTHCS*YYYYYYYYYY*LK,msg 格式,YYYY是15位数字Imei号
     */
-   public static function handle_watch_data_test($client_id, $message)
+   public static function handle_watch_data_test($client_id, $msg)
    {
           static $filename='1.amr';
-          static $tk_flag=false;
-          static $tk_recv_len=0;
-          static $tk_total_len=0;
+          static $len=0;
           static $imei;
-          $len=hexdec(substr($message,0,4));
+          static $message='';
+          static $rev_flag=false;
 
-          if($tk_flag){
-              if($tk_recv_len<$tk_total_len+4){
-                file_put_contents($filename,$message,FILE_APPEND);
-                $tk_recv_len +=strlen($message);
-              }else{
-
-              $tk_flag=false;
-              $tk_recv_len=0;
-              $tk_total_len=0;
-              $rs_tk='CS*'.$imei.'*TK,1';
-              $rs_tk_len=sprintf("%04x",strlen($rs_tk));
-              Gateway::sendToUid($imei,$rs_tk_len.$rs_tk);
-              return;
-            }
+          if($rev_flag){
+            $message .= $msg;
           }else{
-
-          $msg_body=substr($message,4,strlen($message)-4);
-          // if(strlen($msg_body) != $len){
-          //   //return;
-          // }
-
-          $msg_array=explode('*', $msg_body);
-          if(count($msg_array)<3){
-              return;
+             $len=hexdec(substr($msg,0,4));
+             $message=$msg;
           }
-          $type=$msg_array[0];
-          $imei=$msg_array[1];
 
-          $msg_msg=explode(',',$msg_array[2]);
-          $cmd=$msg_msg[0];
 
-          // echo "len= $len \n";
-          // echo "type= $type \n ";
-          // echo "imei= $imei \n ";
-          // echo "cmd= $cmd \n ";
-          // echo "msg_msg= $msg_array[2] \n";
-          //有效数据
-          if($type == 'CS'){
-              Gateway::bindUid($client_id, $imei);
+          if(($len+4) > strlen($message)){
+            $rev_flag=true;
+            return;
+          }else{
+            $rev_flag=false;
+          }
 
-              switch($cmd)
-              {
-                //链路保持
-                case 'LK':
-                  $rs='CS*'.$imei.'*LK';
-                  $rs_len=sprintf("%04x",strlen($rs));
-                  Gateway::sendToUid($imei,$rs_len.$rs);
+          if(($len+4) == strlen($message)){
+
+              $msg_body=substr($message,4,strlen($message)-4);
+
+              $msg_array=explode('*', $msg_body);
+              if(count($msg_array)<3){
                   return;
-                //位置上报
-                case 'UD':
-                  Gateway::sendToUid($imei,"ud");
-                  return;
-                //语音
-                case 'TK': // lencs*imei*tk,amr数据
-                $tk_flag=true;
-                $tk_recv_len +=strlen($message);
-                $tk_total_len=hexdec(substr($message,0,4));
-                $filename=__DIR__.'/amr/'.rand(1,100).'.amr';
-                //$id=$msg_msg[2];
-                //$total=$msg_msg[3];
-                //$amr=$msg_msg[4];
-                $head_len=22;
-                $amr=substr($msg_body,$head_len,strlen($msg_body)-$head_len);
-                file_put_contents($filename,$amr,FILE_APPEND);
-                chmod($filename,0777);
-                if($tk_total_len==($tk_recv_len-4)){
-                  $tk_flag=false;
-                  $tk_recv_len=0;
-                  $tk_total_len=0;
-                  $rs_tk='CS*'.$imei.'*TK,1';
-                  $rs_tk_len=sprintf("%04x",strlen($rs_tk));
-                  Gateway::sendToUid($imei,$rs_tk_len.$rs_tk);
-                  return;
-                }
-                return;
-
-                case 'SYSTEMTIME':
-                  $rs_st='CS*'.$imei.'*SYSTEMTIME,'.time().'000';
-                  $rs_len_st=sprintf("%04x",strlen($rs_st));
-                  Gateway::sendToUid($imei,$rs_len_st.$rs_st);
-                case 'WEATHER':
-                  $rs_wea='CS*'.$imei.'*WEATHER,1';
-                  $rs_len_wea=sprintf("%04x",strlen($rs_st));
-                  Gateway::sendToUid($imei,$rs_len_wea.$rs_wea);
-
-                default:
-                return;
               }
-          }
+              $type=$msg_array[0];
+              $imei=$msg_array[1];
+
+              $msg_msg=explode(',',$msg_array[2]);
+              $cmd=$msg_msg[0];
+
+              //有效数据
+              if($type == 'CS'){
+                  Gateway::bindUid($client_id, $imei);
+
+                  switch($cmd)
+                  {
+                    //链路保持
+                    case 'LK':
+                      $rs='CS*'.$imei.'*LK';
+                      $rs_len=sprintf("%04x",strlen($rs));
+                      Gateway::sendToUid($imei,$rs_len.$rs);
+                      return;
+                    //位置上报
+                    case 'UD':
+                      Gateway::sendToUid($imei,"ud");
+                      return;
+                    //语音
+                    case 'TK': // lencs*imei*tk,amr数据
+                      $filename=__DIR__.'/amr/'.rand(1,100).'.amr';
+                      $head_len=22;
+                      $amr=substr($msg_body,$head_len,strlen($msg_body)-$head_len);
+                      file_put_contents($filename,$amr,FILE_APPEND);
+                      $rs_tk='CS*'.$imei.'*TK,1';
+                      $rs_tk_len=sprintf("%04x",strlen($rs_tk));
+                      Gateway::sendToUid($imei,$rs_tk_len.$rs_tk);
+                      return;
+
+                    case 'SYSTEMTIME':
+                      $rs_st='CS*'.$imei.'*SYSTEMTIME,'.time().'000';
+                      $rs_len_st=sprintf("%04x",strlen($rs_st));
+                      Gateway::sendToUid($imei,$rs_len_st.$rs_st);
+                    case 'WEATHER':
+                      $rs_wea='CS*'.$imei.'*WEATHER,1';
+                      $rs_len_wea=sprintf("%04x",strlen($rs_wea));
+                      Gateway::sendToUid($imei,$rs_len_wea.$rs_wea);
+
+                    default:
+                    return;
+                  }
+              }
         }
+        $message='';
+
    }
 
 
