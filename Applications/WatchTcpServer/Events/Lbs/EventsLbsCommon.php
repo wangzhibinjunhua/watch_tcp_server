b07a357d78ed10b95ba251f0b17bcaef51f55459
wzb <wangzhibin_x@qq.com>
2016-08-19 16:29:20 +0800

    parse ud data

--- Applications/WatchTcpServer/Events/Lbs/EventsLbsCommon.php
+++ Applications/WatchTcpServer/Events/Lbs/EventsLbsCommon.php
@@ -16,7 +16,10 @@ class EventsLbsCommon
 	 */
 	public static function parse($data)
 	{
-		echo $data.__FILE__;
+		$data_arr=explode(',', $data);
+		$imei=substr($string, $data_arr[0]);
+		echo $imei.__FILE__;
 	}
+	
 
 }
