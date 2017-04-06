a0bae649e775b89d53706dbdeef986615cf13b6d
wzb <wangzhibin_x@qq.com>
2017-04-06 17:59:14 +0800

    modify watersales channle port

--- Applications/WaterSales/start_channel.php
+++ Applications/WaterSales/start_channel.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/../../Workerman/Autoloader.php';
 require_once __DIR__ . '/Channel/src/Server.php';
 
 // 不传参数默认是监听0.0.0.0:2206
-$channel_server = new Channel\Server('0.0.0.0',2206);
+$channel_server = new Channel\Server('0.0.0.0',2209);
 // 如果不是在根目录启动，则运行runAll方法
 if(!defined('GLOBAL_START')) {
     Worker::runAll();
