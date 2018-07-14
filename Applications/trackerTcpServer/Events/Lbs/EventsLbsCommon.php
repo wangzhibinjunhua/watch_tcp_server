444a37e619b94d16aa0032fb67a924c94c24f10f
wzb <wangzhibin_x@foxmail.com>
2018-07-14 13:56:16 +0800

    add tracker project

---
+++ Applications/trackerTcpServer/Events/Lbs/EventsLbsCommon.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ *定位数据处理类
+ *
+ */
+namespace Events\Lbs;
+use \GatewayWorker\Wtools\CUrl;
+use \GatewayWorker\Lib\Db;
+
+//test
+//$data = "CS*201508220451111*UD,040816,123951,V,0,N,0,E,0,0,0.0000000,0,4,100,0,0,00000000,1,255,460,0,10145,3682,5,1,TP-LINK415,b0:d5:9d:5d:43:6e,-72";
+//EventsLbsCommon::parse($data);
+
+// $json='{"ret":200,"data":{"status":"1","info":"OK","infocode":"10000","result":{"type":"4","location":"113.9760327,22.559372","radius":"550","desc":"广东省 深圳市 南山区 龙珠五路 靠近桃源派出所(龙珠五路)","country":"中国","province":"广东省","city":"深圳市","citycode":"0755","adcode":"440305","road":"龙珠五路","street":"龙珠五路","poi":"桃源派出所(龙珠五路)"}},"msg":""}';
+// $rs=json_decode($json,true);
+// var_dump($rs);
+// var_dump('222'.$rs['data']['result']['type']);
+$redis=new \Redis();
+
+class EventsLbsCommon {
+
+
+	/**
+	 * [解析定位数据]
+	 *
+	 * @author wzb<wangzhibin_x@qq.com>
+	 *         @DateTime 2016-08-18T11:45:54+0800
+	 * @return [type] [description]
+	 */
+	public static function parse($data) {
+		$ud_content=$data;
+		$data_arr = explode ( ',', $data );
+		var_dump ( $data_arr );
+		$imei = substr ( $data_arr [0], 3, 15 );
+		$date = $data_arr [1];
+		$time = $data_arr [2];
+		$watch_time = substr ( $date, 4, 2 ) . '-' . substr ( $date, 2, 2 ) . '-' . substr ( $date, 0, 2 ) . ' ' . substr ( $time, 0, 2 ) . ':' . substr ( $time, 2, 2 ) . ':' . substr ( $time, 4, 2 );
+		$battery = $data_arr [13];
+		$watch_status = $data_arr [16];
+
+		$state_num = $data_arr [17];
+		$mcc = $data_arr [19];
+		$mnc=$data_arr[20];
+		$lac=$data_arr[21];
+		$cellid=$data_arr[22];
+		$signle=$data_arr[23];
+		$bts_others='';
+		//for($i=1;$i<$state_num;$i++){
+		//	$id=24+($i-1)*3;
+		//	$bts_others .=$mcc.','.$mnc.','.$data_arr[$id].','.$data_arr[$id+1].','.$data_arr[$id+2].'|';
+		//}
+		$bts_main=$mcc.','.$mnc.','.$lac.','.$cellid.','.$signle;
+
+		$wifi_num=$data_arr[24+($state_num-1)*3];
+		$wifi_info='';
+		//for($j=1;$j<=$wifi_num;$j++){
+		//	$wifi_index=25+($state_num-1)*3+($j-1)*3;
+		//	$wifi_info.=$data_arr[$wifi_index+1].','.$data_arr[$wifi_index+2].','.$data_arr[$wifi_index].'|';
+		//}
+
+		$gps_lat='';
+		$gps_lon='';
+		$location_lat='';
+		$location_lon='';
+		$location_type='';
+		$location_content='';
+
+		$gps_status = $data_arr [3];
+		echo "gps status=".$gps_status.PHP_EOL;
+		if ($gps_status == 'A') {
+			// gps定位
+			if($data_arr[5] == 'S'){
+				$location_lat=$gps_lat=0-$data_arr[4];
+			}else{
+				$location_lat=$gps_lat=$data_arr[4];
+			}
+
+			if($data_arr[7] == 'E'){
+				$location_lon=$gps_lon=0-$data_arr[6];
+			}else{
+				$location_lon=$gps_lon=$data_arr[6];
+			}
+			$location_type=0;
+
+		}else if ($gps_status == 'V') {
+			// 基站wifi定位
+			$result=self::get_lbs($bts_main,$bts_others, $wifi_info);
+			if($result){
+				$rs=json_decode($result,true);
+				$rs_type=$rs['data']['result']['type'];
+				if($rs_type == 3){
+					//wifi定位
+					$location_type=1;
+				}else if($rs_type == 4){
+					//基站定位
+					$location_type=2;
+				}else{
+					$location_type=$rs_type;
+				}
+				if($location_type != 0){
+					//$location=$rs['data']['result']['location'];
+					//$location_lon=explode(',', $location)[0];
+					//$location_lat=explode(',', $location)[1];
+					//$location_content=$rs['data']['result']['desc'];
+				}
+			}
+			//echo $result.PHP_EOL;
+		}
+
+		//insert to sql
+// 		$db_watch=Db::instance('db_watch');
+ 		$sys_time=date("Y-m-d H:i:s");
+		// $sql="insert into watch_info (imei,gps_lon,gps_lat,watch_time,
+		// 		system_time,location_lon,location_lat,location_content,
+		// 		location_type,ud_content,battery) values ('$imei','$gps_lon',
+		// 		'$gps_lat','$watch_time','$sys_time','$location_lon','$location_lat',
+		// 		'$location_content','$location_type','$ud_content','$battery')";
+		// echo $sql.PHP_EOL;
+		// echo " ".PHP_EOL;
+// 		$db_watch->query($sql);
+ 		$unix_time=time();
+ 		$msg=" '$imei','$gps_lon','$gps_lat','$watch_time','$sys_time','$location_lon','$location_lat','$location_content','$location_type','$ud_content','$battery','$unix_time'";
+
+		self::save_db($msg);
+	}
+
+
+	/**
+	* @author wzb<wangzhibin_x@foxmail.com>
+	* @date Aug 19, 2016 6:30:54 PM
+	* 调用高德api 使用基站wifi定位
+	*/
+	public static function get_lbs($bts,$nearbts,$wifi_info)
+	{
+		$curl=new CUrl();
+		$api='http://lib.huayinghealth.com/lib-x/?service=lbs.data&';
+		$url=$api.'bts='.$bts.'&nearbts='.$nearbts.'&macs='.$wifi_info;
+		//echo $url.PHP_EOL;
+		return $curl->get($url);
+	}
+
+
+	/**
+	* @author wzb<wangzhibin_x@foxmail.com>
+	* @date Aug 21, 2016 11:43:29 AM
+	* 存入数据库
+	*/
+	public static function save_db($msg)
+	{
+		$redis_watch=new \Redis();
+		$redis_watch->connect('127.0.0.1',6379);
+		$redis_watch->auth("huayingtek2016");
+		//echo $msg;
+		$redis_watch->rPush("watch_info",$msg);
+		$redis_watch->close();
+	}
+}
---
+++ Applications/trackerTcpServer/HandleData.php
@@ -0,0 +1,281 @@
+<?php
+use \GatewayWorker\Lib\Gateway;
+use Events\Lbs\EventsLbsCommon;
+use Events\WeatherService\WeatherService;
+use Workerman\Connection\AsyncTcpConnection;
+use \GatewayWorker\Lib\Db;
+class HandleData {
+	
+	
+	
+	public static function pack_data($data) {
+		$data_len = sprintf ( "%04x", strlen ( $data ) );
+		return $data_len . $data;
+	}
+
+	public static function async($user_id,$task_data,$ext=null)
+	{
+		$task_connection=new AsyncTcpConnection('Text://127.0.0.1:7272');
+		$task_connection->send($task_data);
+		$task_connection->onMessage=function($task_connection,$task_result)use($user_id)
+		{
+			//echo "task_result:".$task_result.PHP_EOL;
+			if(!empty($task_result)){
+				$json=json_decode($task_result,true);
+				if($json){
+					foreach ($json as $arr){
+						foreach ($arr as $tel) {
+
+							//echo $tel.PHP_EOL;
+							$tk_notiy=array('id'=>$tel,'cmd'=>'TK','info'=>1);
+							Gateway::sendToUid($tel,self::pack_data(json_encode($tk_notiy)));
+						}
+					}
+
+				}else{
+					Gateway::sendToUid($user_id,$task_result);
+				}
+			}
+			$task_connection->close();
+		};
+		$task_connection->connect();
+	}
+
+
+	/**
+	* @author wzb<wangzhibin_x@foxmail.com>
+	* @date Sep 6, 2016 2:37:37 PM
+	* 处理异步任务数据
+	*/
+	public static function handle_async_data($connection,$message)
+	{
+		$result='';
+		$msg_array = explode ( '*', $message );
+		//echo count($msg_array).PHP_EOL;
+		$msg_msg = explode ( ',', $msg_array[2] );
+		$cmd = $msg_msg [0];
+		$imei = $msg_array [1];
+		$media_type=0;
+		switch($cmd){
+			case 'WEATHER':
+				$weather_service = new WeatherService ();
+				$rs_weather = $weather_service->parse ( $message );
+				$rs_wea = 'CS*' . $imei . '*WEATHER,';
+				$result=self::pack_data($rs_wea . $rs_weather);
+				break;
+			case 'UD':
+				$ud_parse = new EventsLbsCommon ();
+				$ud_parse->parse ( $message );
+				break;
+			case 'TK':
+				//存入数据库
+				$filename=$msg_msg[1];
+				$media_type='0';
+				//echo $filename.PHP_EOL;
+				$db=Db::instance('db_watch');
+				$app_user=$db->select('app_id')->from('watch_app_watch')->where("watch_imei=$imei")->query();
+				//var_dump($app_user);
+				$result=json_encode($app_user);
+				$sys_time=date("Y-m-d H:i:s");
+				foreach ($app_user as $arr){
+					foreach ($arr as $tel) {
+						$db->insert('watch_message')->cols(array('type'=>$media_type,'user_id'=>$tel,'imei'=>$imei,'file'=>$filename,'stamp'=>time(),'datetime'=>$sys_time))->query();
+
+					}
+				}
+
+				break;
+			case 'PHOTO':
+				//存入数据库
+				$filename=$msg_msg[1];
+				$media_type='1';
+				//echo $filename.PHP_EOL;
+				$db=Db::instance('db_watch');
+				$sys_time=date("Y-m-d H:i:s");
+				$app_user=$db->select('app_id')->from('watch_app_watch')->where("watch_imei=$imei")->query();
+				//var_dump($app_user);
+				$result=json_encode($app_user);
+				foreach ($app_user as $arr){
+					foreach ($arr as $tel) {
+						$db->insert('watch_message')->cols(array('type'=>$media_type,'user_id'=>$tel,'imei'=>$imei,'file'=>$filename,'stamp'=>time(),'datetime'=>$sys_time))->query();
+
+					}
+				}
+				break;
+			default:
+				break;
+		}
+		$connection->send($result);
+	}
+
+	/**
+	 * [handle_watch_data description]
+	 *
+	 * @author wzb<wangzhibin_x@qq.com>
+	 *         @DateTime 2016-07-11T20:05:24+0800
+	 *         @ 处理手表终端发送过来的数据
+	 *         LENGTHCS*YYYYYYYYYY*LK,msg 格式,YYYY是15位数字Imei号
+	 */
+	public static function handle_watch_data($client_id, $message) {
+		static $filename = '1.amr';
+		static $imei;
+
+		// echo $message.' xx'.PHP_EOL;
+		//for debug
+		if(true){
+			$debug_info=$message.' --'.date("Y-m-d H:i:s");
+			Gateway::sendToGroup('debug2',$debug_info);
+		}
+		//end
+
+		$msg_array = explode ( '*', $message );
+		if (count ( $msg_array ) < 3) {
+			return;
+		}
+		$type = $msg_array [0];
+		$imei = $msg_array [1];
+
+		$msg_msg = explode ( ',', $msg_array [2] );
+		$cmd = $msg_msg [0];
+
+		Gateway::bindUid ( $client_id, $imei );
+
+		switch ($cmd) {
+			// 链路保持
+			case 'LK' :
+				$rs_lk = 'CS*' . $imei . '*LK';
+				// $rs_lk_len=sprintf("%04x",strlen($rs));
+				Gateway::sendToUid ( $imei, self::pack_data ( $rs_lk ) );
+				return;
+			// 位置上报
+			case 'UD' :
+				$rs_ud = 'CS*' . $imei . '*UD';
+				// $rs_ud_len=sprintf("%04x",strlen($rs_ud));
+				Gateway::sendToUid ( $imei, self::pack_data ( $rs_ud ) );
+				//$ud_parse = new EventsLbsCommon ();
+				//$ud_parse->parse ( $message );
+				//用异步任务处理
+				self::async($imei,$message);
+				return;
+			// 语音
+			case 'TK' : // lencs*imei*tk,amr数据
+				if(strlen($message) == 23){
+					return;
+				}
+				//echo "tk".PHP_EOL;
+				$rs_tk = 'CS*' . $imei . '*TK,1';
+				// $rs_tk_len=sprintf("%04x",strlen($rs_tk));
+				Gateway::sendToUid ( $imei, self::pack_data ( $rs_tk ) );
+				$filename=$imei.'_'.time(). '.amr';
+				$filepath = '/var/www/html/core/media/childwatch/' . $filename;
+				$head_len = 22;
+				$amr = substr ( $message, $head_len, strlen ( $message ) - $head_len );
+				file_put_contents ( $filepath, $amr, FILE_APPEND );
+				//异步处理录音文件
+				$async_msg='CS*'.$imei.'*TK,'.$filename;
+				self::async($imei,$async_msg);
+				return;
+
+			case 'SYSTEMTIME' :
+				$rs_st = 'CS*' . $imei . '*SYSTEMTIME,' . time () . '000';
+				// $rs_st_len=sprintf("%04x",strlen($rs_st));
+				Gateway::sendToUid ( $imei, self::pack_data ( $rs_st ) );
+				return;
+			case 'WEATHER' :
+				$rs_wea = 'CS*' . $imei . '*WEATHER,';
+				// $rs_wea_len=sprintf("%04x",strlen($rs_wea));
+				Gateway::sendToUid ( $imei, self::pack_data ( $rs_wea . '1' ) );
+
+// 				$weather_service = new WeatherService ();
+// 				$rs_weather = $weather_service->parse ( $message );
+// 				Gateway::sendToUid ( $imei, self::pack_data ( $rs_wea . $rs_weather ) );
+				//采用异步任务处理curl耗时任务
+				self::async($imei,$message);
+				return;
+			case 'PHOTO':
+				$photo_header=25;
+				$photo_jpg=substr ( $message, $photo_header, strlen ( $message ) - $photo_header );
+				$p_filename=$imei.'_'.time(). '.jpg';
+				$p_filepath = '/var/www/html/core/media/childwatch/' . $p_filename;
+				file_put_contents ( $p_filepath, $photo_jpg, FILE_APPEND );
+				$rs_p='CS*' . $imei . '*PHOTO,1';
+				Gateway::sendToUid ( $imei, self::pack_data ( $rs_p ) );
+				//异步处理
+				$async_p_msg='CS*'.$imei.'*PHOTO,'.$p_filename;
+				self::async($imei,$async_p_msg);
+				// if($msg_msg[1]==0){
+				// 	//手表主动拍照上传
+				// 	$rs_p='CS*' . $imei . '*PHOTO,1';
+				// 	Gateway::sendToUid ( $imei, self::pack_data ( $rs_p ) );
+				// }else if($msg_msg[1]==1){
+				// 	//app控制手表拍照上传	,服务器不用回复手表
+				// }
+				return;
+			case 'TEST':
+				$rs_test=array('id'=>'12345678901','cmd'=>'test','info'=>'hahah123');
+				//echo json_encode($rs_test).PHP_EOL;
+				Gateway::sendToUid ( '12345678901', self::pack_data ( json_encode($rs_test)) );
+				return;
+				break;
+			default :
+				return;
+		}
+	}
+
+	/**
+	* @author wzb<wangzhibin_x@foxmail.com>
+	* @date Sep 6, 2016 4:09:09 PM
+	* $message 为json数据包{"id":"","cmd":"","imei":"","info":""}
+	* amr音频文件转为base64
+	*/
+	public static function handle_server_data($client_id, $message) {
+		$message_data = json_decode ( $message, true );
+		if(!$message_data){
+			return;
+		}
+		$id=$message_data['id'];
+		Gateway::bindUid ( $client_id, $id );
+		//echo $message_data ['cmd'] . PHP_EOL;
+		switch ($message_data ['cmd']) {
+			case 'ping':
+				Gateway::sendToUid($id, self::pack_data($message));
+				return;
+				break;
+
+			case 'tk':
+				//判断imei是否在线
+				//echo '## tk'.PHP_EOL;
+				$imei=$message_data['imei'];
+				$amr=base64_decode($message_data['info']);
+				//file_put_contents("1234.amr",$amr,FILE_APPEND);
+				if(!Gateway::isUidOnline($imei)){
+					$rs_tk=array('id'=>$id,'cmd'=>'tk','imei'=>$imei,'info'=>'offline');
+					Gateway::sendToUid($id, self::pack_data(json_encode($rs_tk)));
+				}else{
+					$rs_tk='CS*'.$imei.'*TK,'. $amr;
+					Gateway::sendToUid($imei, self::pack_data($rs_tk));
+					$rs_tk1=array('id'=>$id,'cmd'=>'tk','imei'=>$imei,'info'=>'ok');
+					Gateway::sendToUid($id, self::pack_data(json_encode($rs_tk1)));
+				}
+
+				break;
+			case 'test' :
+				if ($message_data ['info'] == 'tk') {
+					$file = file_get_contents ( 'test.amr' );
+					$rs = 'CS*201508220452222*TK,' . $file;
+					// $rs_len=sprintf("%04x",strlen($rs));
+					Gateway::sendToAll ( self::pack_data ( $rs ) );
+				} else {
+					$tmsg=$message_data ['info'];
+					$tmsg_array = explode ( '*', $tmsg );
+					$timei=$tmsg_array[1];
+					//Gateway::sendToAll ( $message_data ['info'] );
+					Gateway::sendToUid($timei,$tmsg);
+				}
+				break;
+			default :
+				// code...
+				break;
+		}
+	}
+}
